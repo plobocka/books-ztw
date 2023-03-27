e661511f5c10c3c0ec7dabc9d74bc1c214a4f839
Paulina <pjlobocka@gmail.com>
2023-03-28 00:12:34 +0200

    Changes by maciek

--- src/main/java/pl/edu/pwr/ztw/controllers/BooksControler.java
+++ src/main/java/pl/edu/pwr/ztw/controllers/BooksControler.java
@@ -3,10 +3,11 @@ package pl.edu.pwr.ztw.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.pwr.ztw.services.IBooksService;
+import pl.edu.pwr.ztw.model.Book;
+
+import java.util.List;
 
 @RestController
 public class BooksControler {
