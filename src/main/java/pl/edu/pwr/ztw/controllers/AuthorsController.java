718dd88db1d6fbcab6c80f147cfc6b8f9e27b957
Mateusz Bieniek <mbieniek396@gmail.com>
2023-03-28 15:04:48 +0200

    GIT

--- src/main/java/pl/edu/pwr/ztw/controllers/AuthorsController.java
+++ src/main/java/pl/edu/pwr/ztw/controllers/AuthorsController.java
@@ -1,12 +1,45 @@
 package pl.edu.pwr.ztw.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pl.edu.pwr.ztw.model.Author;
+import pl.edu.pwr.ztw.model.Book;
 import pl.edu.pwr.ztw.services.IAuthorService;
 import pl.edu.pwr.ztw.services.IBooksService;
 
 @RestController
 public class AuthorsController {
+    @Autowired
+    IAuthorService authorService;
 
+    @RequestMapping(value = "/authors", method = RequestMethod.GET)
+    public ResponseEntity<Object> getAuthors() {
+        return new ResponseEntity<>(authorService.getAuthors(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/author/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Object> getAuthor(@PathVariable("id") int id) {
+        return new ResponseEntity<>(authorService.getAuthor(id), HttpStatus.OK);
+    }
+
+    @PostMapping("/author")
+    public ResponseEntity<Author> handleRequest(@RequestBody Author author) {
+        Author responseAuthor = authorService.addAuthor(author);
+        return new ResponseEntity(responseAuthor, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/author")
+    public ResponseEntity<Author> handlePutAuthorRequest(@RequestBody Author author) {
+        Author responseAuthor = authorService.updateAuthor(author.getId(), author);
+        return new ResponseEntity(responseAuthor, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/author/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteAuthor(@PathVariable("id") int id) {
+        authorService.deleteAuthor(id);
+        return new ResponseEntity<>(authorService.getAuthor(id), HttpStatus.OK);
+    }
 
 }
--- src/main/java/pl/edu/pwr/ztw/services/AuthorService.java
+++ src/main/java/pl/edu/pwr/ztw/services/AuthorService.java
@@ -2,9 +2,12 @@ package pl.edu.pwr.ztw.services;
 
 import org.springframework.stereotype.Service;
 import pl.edu.pwr.ztw.model.Author;
+import pl.edu.pwr.ztw.model.Book;
 import pl.edu.pwr.ztw.repo.Repository;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Service
 public class AuthorService implements IAuthorService {
@@ -25,22 +28,38 @@ public class AuthorService implements IAuthorService {
     }
 
     @Override
-    public void addAuthor(Author author) {
+    public Author addAuthor(Author author) {
         repo.authorsRepo.add(author);
+        return author;
     }
 
     @Override
     public void deleteAuthor(int id) {
+        List<Book> books= repo.booksRepo;
+        int searchID = 0;
+        for (Book book : books) {
+            for (int i=0; i < book.getAuthors().size(); i++){
+                if (book.getAuthors().get(i).getId() == id){
+                    searchID = i;
+                }
+
+            }
+            List<Author> as = new ArrayList<>(book.getAuthors());
+            as.remove(searchID);
+            book.setAuthors(as);
+        }
+
         repo.authorsRepo.removeIf(a -> a.getId() == id);
     }
 
     @Override
-    public void updateAuthor(int id, Author updatedAuthor) {
+    public Author updateAuthor(int id, Author updatedAuthor) {
         Author author = repo.authorsRepo.stream()
                 .filter(a -> a.getId() == id)
                 .findAny()
                 .orElse(null);
         author.setFirstName(updatedAuthor.getFirstName());
         author.setLatsName(updatedAuthor.getLatsName());
+        return author;
     }
 }
